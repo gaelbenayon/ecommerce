a4346bb34c8c3dd47ba48338b10fb612fe9d4c24
gaelbenayon <gaelbenayon@gmail.com>
2023-04-26 22:41:59 -0300

    clase1
    prompt
    alert
    .toFixed
    parseFloat

--- js/main.js
+++ js/main.js
@@ -1,1 +1,6 @@
-console.log("hola mundo");
+let = peso = parseFloat(prompt("Introduzca el peso:"));
+let = altura = parseFloat(prompt("Introduzca la altura:"));
+
+let = IMC = peso / (altura*altura).toFixed(2); //toFixed corta los números después de la coma
+
+alert("El IMC es " + IMC.toFixed(2));
