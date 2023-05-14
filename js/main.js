a99fd7e66c6a1923edfad9acdd1fdf5f6fa69395
Gael Franco Benayon <gaelbenayon@gmail.com>
2023-05-14 01:17:37 -0300

    PreEntrega1

--- js/main.js
+++ js/main.js
@@ -1,88 +1,104 @@
 //CALCULADORA DE PRECIOS (IVA, CUPONES DE DESCUENTO, FINANCIACIÓN EN CUOTAS)
 
 let cantidadPrecios = parseInt(prompt("Bienvenido/a a la calculadora de precios finales con IVA, descuentos y financiación en cuotas \n \n ¿Cuántos valores ingresará?"));
+let precioSinIVA = parseFloat(prompt("Ingrese el valor del producto sin IVA:"));
+let codigoDescuento = prompt("Ingrese su código de descuento o presione enter para avanzar al siguiente paso: \n \n 10OFF \n 25OFF \n 50OFF \n 2X1").toUpperCase();
+let cantidadCuotas = parseInt(prompt("¿En cuántas cuotas va a financiar el pago? \n \n 1 (sin interés) \n 3 (+25%) \n 6 (+60%)"));
 
-for (i=1; i <= cantidadPrecios; i++) {
-    if (cantidadPrecios === i) {
-        calcularIVA();
-        confirm("¿Quiere calcular otro producto más?") ? calcularIVA() : alert("Gracias por operar con nosotros");
-    } else {
-        calcularIVA();
-    }
-}
-
-function calcularIVA() {
-    let precioSinIVA = parseFloat(prompt("Ingrese el valor del producto sin IVA:"));
-    let precioConIVA = precioSinIVA * 1.21;
+const calcularIVA = function(montoSinIVA) {
+    let precioConIVA = montoSinIVA * 1.21;
     if (precioConIVA > 0) {
-        calcularDescuento(precioConIVA);
+        return precioConIVA;
     } else {
         alert("Ingrese un valor mayor a $0");
-        calcularIVA();
+        calcularIVA(precioSinIVA);
     }
 }
 
-function calcularDescuento(valorConIVA) {
-    let codigoDescuento = prompt("Ingrese su código de descuento o presione enter para avanzar al siguiente paso: \n \n 10OFF \n 25OFF \n 50OFF \n 2X1").toUpperCase();
+const calcularDescuento = function(valorConIVA,cupon) {
     let precioFinal;
     let productoGratis = false;
-    switch (codigoDescuento) {
+    switch (cupon) {
         case "10OFF":
             precioFinal = valorConIVA * 0.90;
-            calcularCuotas(precioFinal);
             break;
         case "25OFF":
             precioFinal = valorConIVA * 0.75;
-            calcularCuotas(precioFinal);
             break;
         case "50OFF":
             precioFinal = valorConIVA * 0.50;
-            calcularCuotas(precioFinal);
             break;
         case "2X1":
             precioFinal = valorConIVA;
             productoGratis = true;
-            calcularCuotas(precioFinal);
-            // alert("Te llevás un producto gratis :)")
             break;
         default:
-            alert("No has ingresado ningún cupón válido")
+            alert("No has ingresado ningún cupón válido");
             precioFinal = valorConIVA;
-            calcularCuotas(precioFinal);
             break;
     } 
+    return precioFinal;
 }
 
-function calcularCuotas(valor) {
-    let cantidadCuotas = parseInt(prompt("¿En cuántas cuotas va a financiar el pago? \n \n 1 (sin interés) \n 3 (+25%) \n 6 (+60%)"));
+const calcularFinanciacion = function(valor) {
     let precioFinanciado;
     let interes;
-    let valorCuotas;
     switch (cantidadCuotas) {
         case 1:
-            precioFinanciado = valor;
-            valorCuotas = precioFinanciado;
-            informarPrecioFinal(precioFinanciado, cantidadCuotas, valorCuotas);
+            interes = 1;
+            precioFinanciado = valor * interes;
             break;
         case 3:
             interes = 1.25;
             precioFinanciado = valor * interes;
-            valorCuotas = precioFinanciado / cantidadCuotas;
-            informarPrecioFinal(precioFinanciado, cantidadCuotas, valorCuotas);
             break;
         case 6:
             interes = 1.6;
             precioFinanciado = valor * interes;
-            valorCuotas = precioFinanciado / cantidadCuotas;
-            informarPrecioFinal(precioFinanciado, cantidadCuotas, valorCuotas);
             break;
         default:
             alert("Ingrese una financiación aceptada");
-            calcularCuotas(valor);
+            cantidadCuotas = parseInt(prompt("¿En cuántas cuotas va a financiar el pago? \n \n 1 (sin interés) \n 3 (+25%) \n 6 (+60%)"));
+            calcularFinanciacion(calcularDescuento(calcularIVA(precioSinIVA),codigoDescuento));
             break;
+    }
+    return precioFinanciado;
+}
+
+const calcularValorCuota = function(financiacion,cuotas) {
+    let valorCuotas = financiacion / cuotas;
+    return valorCuotas;
+}
+
+const informarTotal = function(precio,precioPorCuota) {
+    alert("El precio final, con IVA y descuentos (si aplican), es de $" + precio.toFixed(2) + "\n \nLa financiación es de " + cantidadCuotas + " cuota/s de $" + precioPorCuota.toFixed(2));
+}
+
+for (i=1; i <= cantidadPrecios; i++) {
+    if (cantidadPrecios === i) {
+        calculadora();
+        confirm("¿Quiere calcular otro producto más?") ? repetirCalculadora() : alert("Gracias por operar con nosotros");
+    } else {
+        calculadora();
+        precioSinIVA = parseFloat(prompt("Ingrese el valor del producto sin IVA:"));
+        codigoDescuento = prompt("Ingrese su código de descuento o presione enter para avanzar al siguiente paso: \n \n 10OFF \n 25OFF \n 50OFF \n 2X1").toUpperCase();
+        cantidadCuotas = parseInt(prompt("¿En cuántas cuotas va a financiar el pago? \n \n 1 (sin interés) \n 3 (+25%) \n 6 (+60%)"));
     }
 }
 
-function informarPrecioFinal(precio,cantCuota,valorCuota) {
-    alert("El precio final, con IVA y descuentos (si aplican), es de $" + precio.toFixed(2) + "\n \nLa financiación es de " + cantCuota + " cuota/s de $" + valorCuota.toFixed(2))
+function calculadora() {
+    informarTotal(calcularFinanciacion(calcularDescuento(calcularIVA(precioSinIVA),codigoDescuento)),calcularValorCuota(calcularFinanciacion(calcularDescuento(calcularIVA(precioSinIVA),codigoDescuento)),cantidadCuotas));
 }
+
+function repetirCalculadora() {
+    precioSinIVA = parseFloat(prompt("Ingrese el valor del producto sin IVA:"));
+    codigoDescuento = prompt("Ingrese su código de descuento o presione enter para avanzar al siguiente paso: \n \n 10OFF \n 25OFF \n 50OFF \n 2X1").toUpperCase();
+    cantidadCuotas = parseInt(prompt("¿En cuántas cuotas va a financiar el pago? \n \n 1 (sin interés) \n 3 (+25%) \n 6 (+60%)"));
+    informarTotal(calcularFinanciacion(calcularDescuento(calcularIVA(precioSinIVA),codigoDescuento)),calcularValorCuota(calcularFinanciacion(calcularDescuento(calcularIVA(precioSinIVA),codigoDescuento)),cantidadCuotas));
+}
+
+//PARA VERIFICAR DATOS POR SEPARADO EN LA CONSOLA
+// console.log("El valor con IVA es: $" + calcularIVA(precioSinIVA));
+// console.log("El valor con descuento es de $" + calcularDescuento(calcularIVA(precioSinIVA),codigoDescuento));
+// console.log("El valor financiado es de $" + calcularFinanciacion(calcularDescuento(calcularIVA(precioSinIVA),codigoDescuento)));
+// console.log("El valor de cada cuota es de $" + calcularValorCuota(calcularFinanciacion(calcularIVA(precioSinIVA),codigoDescuento),cantidadCuotas));
